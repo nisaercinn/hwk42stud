959bce7753bce546e7fe735b3e06eb5ef90db631
Nisanur Ercin <nisaei92@gmail.com>
2026-01-02 19:49:58 +0100

    satisfying requirement one

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void printRange(int a, int b) {
+        for (int i = a + 1; i < b; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        printRange(x, y);
+    }
+}
