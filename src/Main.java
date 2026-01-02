4a22ccc9e91014c1a0f543636d6d968ddbd537b9
Nisanur Ercin <nisaei92@gmail.com>
2026-01-02 19:53:35 +0100

    satisfying requirement change three

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void printRange(int a, int b) {
-        for (int i = a + 1; i < b; i++) {
+    for (int i = a + 1; i < b; i++) {
+        if (i % 2 == 0) {
             System.out.println(i);
         }
     }
+}
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
