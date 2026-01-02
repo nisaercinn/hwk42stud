64eb69cd4ee74a28e28657ba86bd344fe9474c7d
Nisanur Ercin <nisaei92@gmail.com>
2026-01-02 19:59:54 +0100

    Merge branch 'feature-3'

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void printRange(int a, int b) {
-<<<<<<< HEAD
         for (int i = a + 1; i < b; i++) {
-            if (i % 2 != 0) {
+            if (i % 2 == 0) {
                 System.out.println(i);
             }
-=======
-    for (int i = a + 1; i < b; i++) {
-        if (i % 2 == 0) {
-            System.out.println(i);
->>>>>>> feature-3
         }
     }
-}
-
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
